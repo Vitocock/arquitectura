f138c4d685eaf79a9c51aa01f4ce6f8d648a609e
Tilinger <vmmc100@gmail.com>
2025-06-14 13:55:34 -0400

    Primer commit

---
+++ imc.py
@@ -0,0 +1,72 @@
+def calcular_imc(peso, altura):
+    """Calcula el Índice de Masa Corporal (IMC).
+
+    Args:
+        peso (float): Peso en kilogramos.
+        altura (float): Altura en metros.
+
+    Returns:
+        float: El valor del IMC.
+    """
+    
+    return peso / (altura ** 2)
+
+def clasificar_imc(imc):
+    """Clasifica el IMC en categorías.
+
+    Args:
+        imc (float): El valor del IMC.
+
+    Returns:
+        str: La categoría del IMC.
+    """
+    if imc < 18.5:
+        return 1
+    elif 18.5 <= imc < 24.9:
+        return 2
+    elif 25 <= imc < 29.9:
+        return 3
+    elif 30 <= imc < 34.9:
+        return 4
+    elif 35 <= imc < 39.9:
+        return 5
+    else:
+        return 6
+
+def main():
+    """Función principal para calcular y mostrar el IMC de un curso."""
+    imc_promedio = 0
+    num_estudiantes = int(input("¿Cuántos estudiantes hay en el curso? "))
+    bajo_peso = normal = sobrepeso = obeso_g1 = obeso_g2 = obeso_g3 = 0 # Se puede hacer con arreglo
+
+    for i in range(num_estudiantes):
+        print(f"\n--- Datos del estudiante {i + 1} ---")
+        
+        peso = float(input("Peso en kg (ej. 70.5): "))
+        altura = float(input("Altura en metros (ej. 1.75): "))
+        
+        imc = calcular_imc(peso, altura)
+        clasificacion = clasificar_imc(imc)
+        
+        if clasificacion == 1: bajo_peso += 1
+        elif clasificacion == 2: normal += 1
+        elif clasificacion == 3: sobrepeso += 1
+        elif clasificacion == 4: obeso_g1 += 1
+        elif clasificacion == 5: obeso_g2 += 1
+        elif clasificacion == 6: obeso_g3 += 1
+        
+        imc_promedio += imc
+
+    imc_promedio /= num_estudiantes
+ 
+    print("\n--- Resultados del IMC del Curso ---")
+    print(f"IMC PROMEDIO: {imc_promedio}")
+    print("\n--- Clasificacion del IMC de los estudiantes ---")
+    print(f"Bajo peso: {bajo_peso}")
+    print(f"Peso Normal: {normal}")
+    print(f"Sobrepeso: {sobrepeso}")
+    print(f"Obesidad grado I: {obeso_g1}")
+    print(f"Obesidad grado II: {obeso_g2}")
+    print(f"Obesidad grado III (Mórbida): {obeso_g3}")
+
+main()
---
+++ imc_mars.asm
@@ -0,0 +1,205 @@
+.data
+prompt_n_estudiantes: .asciiz "¿Cuántos estudiantes hay en el curso? "
+prompt_peso:         .asciiz "\nPeso en kg (ej. 70.5): "
+prompt_altura:       .asciiz "Altura en metros (ej. 1.75): "
+newline:             .asciiz "\n"
+resultados:          .asciiz "\n--- Resultados del IMC del Curso ---\n"
+imc_promedio_str:    .asciiz "IMC PROMEDIO: "
+clasificacion_str:   .asciiz "\n--- Clasificacion del IMC de los estudiantes ---\n"
+bajo_peso_str:       .asciiz "Bajo peso: "
+normal_str:          .asciiz "Peso Normal: "
+sobrepeso_str:       .asciiz "Sobrepeso: "
+obeso1_str:          .asciiz "Obesidad grado I: "
+obeso2_str:          .asciiz "Obesidad grado II: "
+obeso3_str:          .asciiz "Obesidad grado III (Mórbida): "
+
+# Constantes flotantes
+f18_5: .float 18.5
+f24_9: .float 24.9
+f25:   .float 25.0
+f29_9: .float 29.9
+f30:   .float 30.0
+f34_9: .float 34.9
+f35:   .float 35.0
+f39_9: .float 39.9
+zero_float: .float 0.0
+
+.text
+.globl main
+main:
+    # Inicializar contadores
+    li $t0, 0       # i = 0
+    li $t1, 0       # bajo_peso
+    li $t2, 0       # normal
+    li $t3, 0       # sobrepeso
+    li $t4, 0       # obeso_g1
+    li $t5, 0       # obeso_g2
+    li $t6, 0       # obeso_g3
+    l.s $f12, zero_float  # imc_promedio = 0.0
+
+    # Leer número de estudiantes
+    li $v0, 4
+    la $a0, prompt_n_estudiantes
+    syscall
+
+    li $v0, 5
+    syscall
+    move $s0, $v0       # num_estudiantes
+
+loop:
+    beq $t0, $s0, done
+
+    # Mostrar prompt peso
+    li $v0, 4
+    la $a0, prompt_peso
+    syscall
+
+    li $v0, 6
+    syscall
+    mov.s $f2, $f0       # peso
+
+    # Mostrar prompt altura
+    li $v0, 4
+    la $a0, prompt_altura
+    syscall
+
+    li $v0, 6
+    syscall
+    mov.s $f4, $f0       # altura
+
+    # IMC = peso / (altura * altura)
+    mul.s $f6, $f4, $f4
+    div.s $f8, $f2, $f6   # imc en $f8
+
+    # Acumular imc_promedio
+    add.s $f12, $f12, $f8
+
+    # Clasificar IMC
+    l.s $f10, f18_5
+    c.lt.s $f8, $f10
+    bc1t clas1
+
+    l.s $f14, f18_5
+    c.lt.s $f14, $f8
+    bc1f clas_next2
+    l.s $f10, f24_9
+    c.lt.s $f8, $f10
+    bc1t clas2
+clas_next2:
+
+    l.s $f14, f25
+    c.lt.s $f14, $f8
+    bc1f clas_next3
+    l.s $f10, f29_9
+    c.lt.s $f8, $f10
+    bc1t clas3
+clas_next3:
+
+    l.s $f14, f30
+    c.lt.s $f14, $f8
+    bc1f clas_next4
+    l.s $f10, f34_9
+    c.lt.s $f8, $f10
+    bc1t clas4
+clas_next4:
+
+    l.s $f14, f35
+    c.lt.s $f14, $f8
+    bc1f clas_next5
+    l.s $f10, f39_9
+    c.lt.s $f8, $f10
+    bc1t clas5
+clas_next5:
+
+    j clas6
+
+clas1:
+    addi $t1, $t1, 1
+    j next
+clas2:
+    addi $t2, $t2, 1
+    j next
+clas3:
+    addi $t3, $t3, 1
+    j next
+clas4:
+    addi $t4, $t4, 1
+    j next
+clas5:
+    addi $t5, $t5, 1
+    j next
+clas6:
+    addi $t6, $t6, 1
+
+next:
+    addi $t0, $t0, 1
+    j loop
+
+done:
+    # Calcular imc_promedio = imc_promedio / num_estudiantes
+    mtc1 $s0, $f14
+    cvt.s.w $f14, $f14
+    div.s $f12, $f12, $f14
+
+    # Mostrar resultados
+    li $v0, 4
+    la $a0, resultados
+    syscall
+
+    li $v0, 4
+    la $a0, imc_promedio_str
+    syscall
+
+    li $v0, 2
+    mov.s $f12, $f12
+    syscall
+
+    li $v0, 4
+    la $a0, clasificacion_str
+    syscall
+
+    li $v0, 4
+    la $a0, bajo_peso_str
+    syscall
+    li $v0, 1
+    move $a0, $t1
+    syscall
+
+    li $v0, 4
+    la $a0, normal_str
+    syscall
+    li $v0, 1
+    move $a0, $t2
+    syscall
+
+    li $v0, 4
+    la $a0, sobrepeso_str
+    syscall
+    li $v0, 1
+    move $a0, $t3
+    syscall
+
+    li $v0, 4
+    la $a0, obeso1_str
+    syscall
+    li $v0, 1
+    move $a0, $t4
+    syscall
+
+    li $v0, 4
+    la $a0, obeso2_str
+    syscall
+    li $v0, 1
+    move $a0, $t5
+    syscall
+
+    li $v0, 4
+    la $a0, obeso3_str
+    syscall
+    li $v0, 1
+    move $a0, $t6
+    syscall
+
+    li $v0, 10
+    syscall
+
